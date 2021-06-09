ad41127ff922fcc7b9bef5fd3925162bc35d10ba
Philippe Gris <philippe.gris@clermont.in2p3.fr>
2021-06-09 14:57:52 +0200

    modified rc parameters for display

--- sn_DD_opti/__init__.py
+++ sn_DD_opti/__init__.py
@@ -4,9 +4,15 @@ import matplotlib.pyplot as plt
 filtercolors = dict(zip('ugrizy', ['b', 'c', 'g', 'y', 'r', 'm']))
 plt.rcParams['xtick.labelsize'] = 20
 plt.rcParams['ytick.labelsize'] = 20
-plt.rcParams['axes.labelsize'] = 20
-plt.rcParams['font.size'] = 20
-plt.rcParams['figure.titlesize'] = 20
-plt.rcParams['legend.fontsize'] = 20
+plt.rcParams['axes.labelsize'] = 22
+plt.rcParams['font.size'] = 22
+plt.rcParams['figure.titlesize'] = 22
+plt.rcParams['legend.fontsize'] = 22
+
+#plt.rcParams['font.family'] = 'Liberation Serif'
+plt.rcParams['font.family'] = 'Arial'
+#plt.rcParams['font.family'] = 'Verdana'
 plt.rcParams['font.weight'] = 'bold'
-plt.rcParams['font.family'] = 'serif'
+
+#plt.rcParams['mathtext.default'] = 'regular'
+#plt.rcParams['font.serif'] = 'Times'
