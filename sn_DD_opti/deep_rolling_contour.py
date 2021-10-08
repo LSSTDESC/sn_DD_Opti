74eebdf91e2dad712618c70685c3009fdc12eaef
Philippe Gris <gris@clermont.in2p3.fr>
2021-10-08 09:10:16 +0200

    decrease the budget fraction for plot

--- sn_DD_opti/deep_rolling_contour.py
+++ sn_DD_opti/deep_rolling_contour.py
@@ -538,7 +538,7 @@ def plotContour(ax, zfields, fDir, fName,
     #             aspect='auto', alpha=0.25, cmap='hsv')
 
     zzv = [0.5, 0.6, 0.7, 0.8, 0.9]
-    zzv = [0.05, 0.07, 0.08, 0.10, 0.12, 0.15]
+    zzv = [0.04, 0.05, 0.07, 0.08, 0.10, 0.12, 0.15]
     CS = ax.contour(NF, ZLIMIT, BUD, zzv, colors='k')
 
     fmt = {}
