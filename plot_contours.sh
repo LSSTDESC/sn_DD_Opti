860d359e01ed47d45698d4b571524b1e92a3bd4b
Philippe Gris <gris@clermont.in2p3.fr>
2022-01-03 17:33:14 +0100

    add a script to plot all the figures

---
+++ plot_contours.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#Early Deep Rolling
+#python sn_DD_opti/deep_rolling_contour.py --cosmoDir cosmo_files_last --cosmoFile cosmoSN_deep_rolling_2_2_mini_0.60_Ny_40 --xaxis nseasons_ultra_unique --yaxis zcomp_ultra_unique --var_to_plot nsn_DD --Ny 40
+#python sn_DD_opti/deep_rolling_contour.py --cosmoDir cosmo_files_last --cosmoFile cosmoSN_deep_rolling_2_2_mini_0.60_Ny_40 --xaxis nseasons_ultra_unique --yaxis zcomp_ultra_unique --var_to_plot sigma_w --Ny 40
+python sn_DD_opti/deep_rolling_contour.py --cosmoDir cosmo_files_last --cosmoFile cosmoSN_deep_rolling_2_2_mini_0.65_Ny_40 --xaxis nseasons_ultra_unique --yaxis zcomp_ultra_unique --var_to_plot sigma_w --Ny 40
+python sn_DD_opti/deep_rolling_contour.py --cosmoDir cosmo_files_last --cosmoFile cosmoSN_deep_rolling_2_2_mini_0.65_Ny_40 --xaxis nseasons_ultra_unique --yaxis zcomp_ultra_unique --var_to_plot nsn_DD --Ny 40
+
+#Deep Rolling 10 Years
+python sn_DD_opti/deep_rolling_contour.py --cosmoDir cosmo_files_last --cosmoFile cosmoSN_deep_rolling_0.80_0.80_2_2_Ny_40 --var_to_plot nsn_DD --Ny 40
+python sn_DD_opti/deep_rolling_contour.py --cosmoDir cosmo_files_last --cosmoFile cosmoSN_deep_rolling_0.80_0.80_2_2_Ny_40 --var_to_plot sigma_w --Ny 40
+
+#universal 
+python sn_DD_opti/deep_rolling_contour.py --cosmoDir cosmo_files_last --cosmoFile cosmoSN_universal_10_Ny_40 --Ny 40 --var_to_plot nsn_DD
+python sn_DD_opti/deep_rolling_contour.py --cosmoDir cosmo_files_last --cosmoFile cosmoSN_universal_10_Ny_40 --Ny 40 --var_to_plot sigma_w
