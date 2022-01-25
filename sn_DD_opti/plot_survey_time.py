74d559f6170cf80f03902f333ccc247fd127c6f0
Philippe Gris <gris@clermont.in2p3.fr>
2022-01-25 17:27:08 +0100

    reshuffling the code

--- sn_DD_opti/plot_survey_time.py
+++ sn_DD_opti/plot_survey_time.py
@@ -6,6 +6,214 @@ from wrapper import DD_Budget
 from scipy.interpolate import interp1d
 
 
+class Budget_Time:
+    """
+    class to estimate for each configuration some infos corresponding to a set of budgets
+
+    Parameters
+    ---------------
+    config: str
+      list of files to process (csv file)
+    budget: list(float), opt
+      list of budget to consider (default: [0.05,0.08,0.1])
+
+
+    """
+
+    def __init__(self, config, cosmoDir='cosmo_files_yearly',
+                 prefix_Nvisits='Nvisits_z_-2.0_0.2_error_model_ebvofMW_0.0_nvisits_Ny',
+                 visitsDir='visits_files', budget=[0.05, 0.08, 0.1]):
+
+        conf = pd.read_csv(config, comment='#')
+        self.cosmoDir = cosmoDir
+        self.prefix_Nvisits = prefix_Nvisits
+        self.visitsDir = visitsDir
+        self.budget = budget
+
+        self.data = self.get_data(conf)
+
+        print(self.data)
+
+    def get_data(self, conf):
+        """
+        Method to load data in a pandas df
+
+        Parameters
+        --------------
+        conf: str
+          configuration file to process (csv file)
+
+        Returns
+        ----------
+        pandas df of data
+
+        """
+        df = pd.DataFrame()
+        for index, row in conf.iterrows():
+            fi = '{}/{}.hdf5'.format(self.cosmoDir, row['cosmoFile'])
+            nName = row['name']
+            ttype = row['type']
+            fload = self.load(fi)
+            fload['confName'] = nName
+            df = pd.concat((df, fload))
+
+        df = self.add_infos(df)
+        df = df.fillna(0)
+        return df
+
+    def load(self, fi):
+        """
+        Method  to load file in pandas df
+
+        Parameters
+        ---------------
+        fi: str
+          name of the file (full path) to be loaded
+
+        Returns
+        ----------
+        pandas df corresponding to the file
+        """
+        df = pd.read_hdf(fi)
+
+        #df['year'] = df['conf'].str.split('_').str.get(1).astype(int)
+        df = df.sort_values(by=['year'])
+
+        # print('ici', df)
+        return df
+
+    def add_infos(self, df):
+        """
+        Method to add infos to df
+
+        Parameters
+        ---------------
+        df: pandas df
+          original df to append
+
+        Returns
+        ----------
+        pandas df with added values
+
+        """
+        Ny = '{}'.format(int(np.median(df['Ny'])))
+
+        fName = '{}_{}.npy'.format(self.prefix_Nvisits, Ny)
+        budget = DD_Budget(self.visitsDir, fName)
+        # add budget colmuns
+        cosmo_bud = budget(df)
+        df = cosmo_bud
+        df['frac_high_z'] = df['nsn_z_09']/df['nsn_DD']
+        df['nsn_DD_ultra'] = df['nsn_DD_COSMOS'] + \
+            df['nsn_DD_XMM-LSS']
+        if 'nsn_ultra' in df.columns:
+            df['nsn_ultra_zless_08'] = df['nsn_ultra'] - \
+                df['nsn_ultra_z_08']
+            df['nsn_dd_zless_05'] = df['nsn_dd']-df['nsn_dd_z_05']
+            df['nsn_ultra_spectro'] = 200*df['year']
+            df['nsn_dd_spectro'] = 200*df['year']
+            df['nsn_spectroid'] = df[['nsn_ultra_zless_08', 'nsn_ultra_spectro']].min(axis=1) +\
+                df[['nsn_dd_zless_05', 'nsn_dd_spectro']].min(axis=1)
+            df['nsn_photid'] = df['nsn_DD']-df['nsn_spectroid']
+            df['frac_photid'] = df['nsn_photid']/df['nsn_DD']
+        df['sigma_w_rel'] = df['sigma_w']/df['w']
+        return df
+
+    def get_infos(self, xvar='year', yvar=['sigma_w', 'nsn_DD'], budget=[0.05, 0.08, 0.10]):
+
+        rtot = []
+
+        print(self.data.columns)
+        self.data = self.data.round(
+            {'zcomp_dd_unique': 2, 'zcomp_ultra_unique': 2})
+        self.data['zcomp_dd_str'] = self.data['zcomp_dd_unique'].astype(
+            str).apply(
+            lambda x: x.ljust(4, '0'))
+
+        self.data['zcomp_ultra_str'] = self.data['zcomp_ultra_unique'].astype(
+            str).apply(lambda x: x.ljust(4, '0'))
+        self.data['confName'] = self.data['confName']+'_' + \
+            self.data['zcomp_ultra_str']+'_'+self.data['zcomp_dd_str']
+        print(self.data[['confName', 'zcomp_dd_str', 'zcomp_ultra_str']])
+
+        self.data = self.modif_early()
+        #confref = 'universal_0.00_0.65'
+        confrefs = ['deep_rolling_early_0.80_0.70']
+        self.plot(confrefs)
+        rr = self.data.groupby(['confName', 'zcomp_dd_str', 'zcomp_ultra_str']).apply(
+            lambda x: self.ana_grp(x)).reset_index()
+
+        return rr
+        """
+        print(rr)
+
+        idx = rr['confName'] == confrefs[0]
+        sel = rr[idx]
+        print(sel[['time_0.05', 'sigma_w_0.05', 'nsn_DD_0.05']])
+        """
+
+    def plot(self, confNames):
+
+        sel = pd.DataFrame()
+        for confName in confNames:
+            idx = self.data['confName'] == confName
+            sel = pd.concat((sel, self.data[idx]))
+        sel = sel.sort_values(by=['year'])
+        fig, ax = plt.subplots()
+        ax.plot(sel['year'], sel['budget'])
+
+        plt.show()
+
+    def modif_early(self):
+
+        idx = self.data['confName'].str.contains("deep_rolling_early")
+
+        sel = self.data[idx]
+        df_new = pd.DataFrame(self.data[~idx])
+        print('alors man', sel['confName'])
+        for confName in sel['confName'].unique():
+            ido = sel['confName'] == confName
+            selb = sel[ido]
+            z_ultra = selb['zcomp_ultra_str'].to_list()[0]
+            z_dd = selb['zcomp_dd_str'].to_list()[0]
+            if z_dd != '0.00':
+                cconfName = '_'.join(confName.split('_')[:3])
+                str_search = '{}_{}_0.00'.format(cconfName, z_ultra)
+                print('searching string', str_search)
+                idata = self.data['confName'] == str_search
+                seldata = self.data[idata]
+                if len(seldata) > 0:
+                    print('yes man', confName, seldata)
+                    dfcp = pd.DataFrame(seldata)
+                    dfcp['confName'] = confName
+                    dfcp['zcomp_ultra_str'] = z_ultra
+                    dfcp['zcomp_dd_str'] = z_dd
+                    dfcon = pd.concat((dfcp, selb))
+                    df_new = pd.concat((df_new, dfcon))
+        return df_new
+
+    def ana_grp(self, grp, xvar='year', yvar=['sigma_w', 'nsn_DD']):
+
+        resdict = {}
+
+        # budget interpolator
+        interp_bud = interp1d(
+            grp['budget'], grp[xvar], bounds_error=False, fill_value=0.)
+        for i, val in enumerate(self.budget):
+            ttime = interp_bud(val)
+            tName = 'time_{}'.format(val)
+            resdict[tName] = [ttime.tolist()]
+            for yv in yvar:
+                vName = '{}_{}'.format(yv, val)
+                interp_var = interp1d(
+                    grp[xvar], grp[yv], bounds_error=False, fill_value=0.)
+                valres = interp_var(ttime)
+                resdict[vName] = [valres.tolist()]
+
+        print(grp.name, resdict)
+        return pd.DataFrame.from_dict(resdict)
+
+
 def plot_new(res, varx='zcomp', vary='sigma_w', budget=[0.05, 0.08, 0.10]):
 
     fig, ax = plt.subplots()
@@ -346,6 +554,16 @@ def gime_data(conf, zcomps=[0.65, 0.70]):
     return df, df_syste
 
 
+def ana_res(infos, xvar='sigma_w', budget=0.05):
+
+    print('hello', infos)
+    vartot = '{}_{}'.format(xvar, budget)
+
+    infos = infos.sort_values(by=[vartot])
+    vvar = ['confName']+[vartot]
+    print(infos[vvar])
+
+
 parser = OptionParser()
 parser.add_option("--visitsDir", type=str, default='visits_files',
                   help="directory where visits files are located[%default]")
@@ -367,6 +585,14 @@ visitsDir = opts.visitsDir
 cadence = opts.cadence
 prefix_Nvisits = opts.prefix_Nvisits
 zcomp = opts.zcomp
+
+bb = Budget_Time(opts.config, cosmoDir=cosmoDir,
+                 prefix_Nvisits=prefix_Nvisits,
+                 visitsDir=visitsDir)
+infos = bb.get_infos()
+
+ana_res(infos)
+print(test)
 
 conf = pd.read_csv(opts.config, comment='#')
 zcomps = [0.55, 0.60, 0.65, 0.70, 0.75, 0.80, 0.85, 0.90]
